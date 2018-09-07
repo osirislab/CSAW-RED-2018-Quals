9588f3c6b2566a95806276b0539bd62b9e6d373c
jmc-linux <johncunniff1248@gmail.com>
2018-09-07 13:57:21 -0400

    web added

---
+++ Web/NYOU_Classes/auth.py
@@ -0,0 +1,100 @@
+from flask import request, redirect, url_for, flash, render_template, session, Blueprint, g
+# from werkzeug.security import check_password_hash, generate_password_hash
+from db import get_db
+import functools
+
+
+bp = Blueprint('auth', __name__, url_prefix='/auth')
+
+
+@bp.route('/register', methods=('GET', 'POST'))
+def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+
+        if not username:
+            error = 'Username is required.'
+        elif not password:
+            error = 'Password is required.'
+        elif db.execute(
+            'SELECT id FROM users WHERE username = "%s"' % username
+        ).fetchone() is not None:
+            error = 'User {} is already registered.'.format(username)
+
+        if error is None:
+            db.execute(
+                'INSERT INTO users (username, password) VALUES ("%s", "%s")' %
+                (username, password)
+            )
+            db.commit()
+            return redirect(url_for('auth.login'))
+
+        flash(error)
+
+    return render_template('auth/register.html')
+
+
+@bp.route('/login', methods=('GET', 'POST'))
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        db = get_db()
+        error = None
+        if db.execute(
+                'SELECT * FROM users WHERE username = "%s" and password = "%s"' %
+                (username, password,)
+        ).fetchone() is not None:
+            user = username
+        else:
+            error = 'Incorrect username or password.'
+        # user = db.execute(
+        #     'SELECT * FROM users WHERE username = "%s"' % username
+        # ).fetchone()
+
+        # if user is None:
+        #     error = 'Incorrect username.'
+        # elif not check_password_hash(user[2], password):
+        #     error = 'Incorrect password.'
+
+        if error is None:
+            session.clear()
+            session['user_id'] = user
+            return redirect(url_for('index'))
+
+        flash(error)
+
+    return render_template('auth/login.html')
+
+
+@bp.route('/logout')
+def logout():
+    session.clear()
+    return redirect(url_for('index'))
+
+
+@bp.before_app_request
+def load_logged_in_user():
+    user_id = session.get('user_id')
+    # print(user_id)
+
+    if user_id is None:
+        g.user = None
+    else:
+        g.user = get_db().execute(
+            'SELECT username FROM users WHERE username = "%s"' % (user_id,)
+        ).fetchone()[0]
+
+
+def login_required(view):
+    @functools.wraps(view)
+    def wrapped_view(**kwargs):
+        if g.user is None:
+            return redirect(url_for('auth.login'))
+
+        return view(**kwargs)
+
+    return wrapped_view
---
+++ Web/NYOU_Classes/sql_init.py
@@ -0,0 +1,36 @@
+class Struct:
+    def __init__(self, **kwargs):
+        for item, value in kwargs.items():
+            self.__setattr__(item, value)  # self.$item = $value
+
+
+USER_SQL = Struct(
+    name='users',
+    sql="""
+    CREATE TABLE users (
+    id INTEGER PRIMARY KEY, 
+    username TEXT, 
+    password TEXT
+    );
+    """
+)
+
+SUBMISSION_SQL = Struct(
+    name='user_submissions',
+    sql="""
+    CREATE TABLE user_submissions (
+    id INTEGER PRIMARY KEY,
+    username TEXT,
+    class_name TEXT,
+    assignment_name TEXT,
+    submission TEXT
+    );
+    """
+)
+
+# CLASSES_SQL = Struct(
+#     name='classes',
+#     sql="""CREATE TABLE classes (name TEXT PRIMARY KEY, assignments TEXT);"""
+# )
+
+table_map = {'users': USER_SQL, 'user_submissions': SUBMISSION_SQL}
