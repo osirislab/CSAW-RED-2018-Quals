e8fde65461b14790b8870f2019be3decb82597c0
chu66yUnic0rn <38295472+chu66yUnic0rn@users.noreply.github.com>
2018-09-18 15:36:40 -0400

    Add files via upload

---
+++ Pwn/make_a_wish/Readme.md
@@ -0,0 +1,23 @@
+#Description
+
+It's my birthday, plz overflow me with wishes :)
+
+??? pts
+
+#Deploying
+
+?????
+
+
+#Challenge
+
+Right amount of buffer changes the integer value to 21, not over-overflow it
+
+#Tested?
+
+###Testing locally
+emmmm...
+
+### Tested remotely
+emmmm...
+
---
+++ Pwn/make_a_wish/source.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+void shellebration(){
+	system("\bin\bash");
+}
+
+int main(int argc, char** argv)
+{
+    printf("Hello, it's my birthday.\n");
+    printf("Say something nice to me and we gonna shellebrate: \n");
+    int yayyy;
+    char buffer[100];
+    yayyy = 0;
+    gets(buffer); 
+    if (yayyy == 21)
+    {
+        shellebration();
+    }
+    else if (yayyy != 21 && yayyy != 0){
+        printf("I am turning 21!");
+    }
+    else
+    {
+        printf("You need to be more extra :)\n");
+    }
+    return 0;
+}
