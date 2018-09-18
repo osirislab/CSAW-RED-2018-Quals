a437cddbfa3e3d3d6d115806d0e77ae2ba9d8121
marcusbarbu <mgb.barbu@gmail.com>
2018-09-18 11:23:17 -0400

    removed superfluous things from mirrormirror

--- Forensics/MirrorMirror/chal.py
+++
@@ -1,7 +0,0 @@
-#all this does is query the IP, just writing this so that it's automatic once everything is set up
-
-import requests
-
-ip_addr = "" #to be determined once we have something registered
-
-r = requests.get(ip_addr)
